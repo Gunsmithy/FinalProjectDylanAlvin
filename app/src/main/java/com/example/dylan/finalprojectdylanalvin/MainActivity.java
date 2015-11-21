d2ada98ec8c0d3881425becbf69df67448558ebd
Gunsmithy <gunsmithy@gmail.com>
2015-11-21 06:42:50 -0500

    Further nav drawer/menu customization Added placeholder fragments for each menu item Settings from menu dropdown also goes to settings nav item

--- app/src/main/java/com/example/dylan/finalprojectdylanalvin/MainActivity.java
+++ app/src/main/java/com/example/dylan/finalprojectdylanalvin/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.dylan.finalprojectdylanalvin;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,7 +16,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        CountdownFragment.OnFragmentInteractionListener,
+        FinderFragment.OnFragmentInteractionListener,
+        TwitterFragment.OnFragmentInteractionListener,
+        YouTubeFragment.OnFragmentInteractionListener,
+        SettingsFragment.OnFragmentInteractionListener,
+        FeedbackFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +30,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +39,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        displayView(R.id.nav_countdown);
     }
 
     @Override
@@ -66,8 +67,9 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        //TODO: Make settings button highlight settings option in nav drawer
         if (id == R.id.action_settings) {
+            displayView(R.id.nav_settings);
             return true;
         }
 
@@ -77,25 +79,84 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        displayView(item.getItemId());
+        return true;
+    }
 
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+    public void displayView(int viewId) {
 
-        } else if (id == R.id.nav_slideshow) {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
 
-        } else if (id == R.id.nav_manage) {
+        switch (viewId) {
+            case R.id.nav_countdown:
+                fragment = new CountdownFragment();
+                title  = getString(R.string.countdownTimer);
+                break;
+            case R.id.nav_finder:
+                fragment = new FinderFragment();
+                title = getString(R.string.theatreFinder);
+                break;
+            case R.id.nav_twitter:
+                fragment = new TwitterFragment();
+                title = getString(R.string.twitterFeed);
+                break;
+            case R.id.nav_youtube:
+                fragment = new YouTubeFragment();
+                title = getString(R.string.youTubeFeed);
+                break;
+            case R.id.nav_settings:
+                fragment = new SettingsFragment();
+                title = getString(R.string.settings);
+                break;
+            case R.id.nav_feedback:
+                fragment = new FeedbackFragment();
+                title = getString(R.string.feedback);
+                break;
+        }
 
-        } else if (id == R.id.nav_share) {
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        }
 
-        } else if (id == R.id.nav_send) {
-
+        // set the toolbar title
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
+    }
+
+    @Override
+    public void onCountdownFragmentInteraction(String string) {
+        // Do stuff
+    }
+
+    @Override
+    public void onFinderFragmentInteraction(String string) {
+        // Do stuff
+    }
+
+    @Override
+    public void onTwitterFragmentInteraction(String string) {
+        // Do stuff
+    }
+
+    @Override
+    public void onYouTubeFragmentInteraction(String string) {
+        // Do stuff
+    }
+
+    @Override
+    public void onSettingsFragmentInteraction(String string) {
+        // Do stuff
+    }
+
+    @Override
+    public void onFeedbackFragmentInteraction(String string) {
+        // Do stuff
     }
 }
